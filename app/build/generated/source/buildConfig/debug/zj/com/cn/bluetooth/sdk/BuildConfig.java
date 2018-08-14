32d56136de3500f7995271d823ce3649c47cfae2
gandara <alejandro.gandara33@gmail.com>
2018-08-14 15:40:36 -0500

    20180811b

--- app/build/generated/source/buildConfig/debug/zj/com/cn/bluetooth/sdk/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/zj/com/cn/bluetooth/sdk/BuildConfig.java
@@ -4,10 +4,10 @@
 package zj.com.cn.bluetooth.sdk;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = false;
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "global.factura.alfilpos";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 4;
-  public static final String VERSION_NAME = "4.0";
+  public static final int VERSION_CODE = 23;
+  public static final String VERSION_NAME = "23.0";
 }
